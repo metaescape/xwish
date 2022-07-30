6ea02b8f2a7ff9d0e90c2524dad755d16e642a1b
pipz <pwy859@163.com>
2022-07-30 10:12:36 +0800

    remove duplicate lines

--- fzf_window.sh
+++ fzf_window.sh
@@ -9,27 +9,14 @@ current_win_id=$(cat /tmp/current_win_id)
 echo "$win" | grep '^/' > /dev/null && { #swap
     id=$(echo $win | tail -1 | cut -d ' ' -f2) #second
     i3-msg "[id=$current_win_id] swap container with id $id"
-    i3-msg "restart" #fix window tree update delay
+    # i3-msg "restart" #fix window tree update delay
     exit;
 } 
 echo "$win" | grep '^,' > /dev/null && { #switch
     id=$(echo $win | tail -1 | cut -d ' ' -f2) #second
     i3-msg [id=$id] focus
 }
-win=$(wmctrl -l | tr -s ' ' | grep -v "select_window\|EmacsAnywhere" |\
-        fzf --height 50%  --header ', to swich; / to swap' --expect=/,,)
 
-[[ -z ${win} ]] && exit;
-echo "$win" | grep '^/' > /dev/null && { #swap
-    id=$(echo $win | tail -1 | cut -d ' ' -f2) #second
-    i3-msg "[id=$current_win_id] swap container with id $id"
-    i3-msg "restart" #fix window tree update delay
-    exit;
-} 
-echo "$win" | grep '^,' > /dev/null && { #switch
-    id=$(echo $win | tail -1 | cut -d ' ' -f2) #second
-    i3-msg [id=$id] focus
-}
 # use enter to select 
 id=$(echo $win | tail -1 | cut -d ' ' -f1) #first
 i3-msg [id=$id] focus
